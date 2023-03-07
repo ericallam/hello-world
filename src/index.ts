e5d16fe106f52ac8ffe1a8beb4f100a55145c50e
Eric Allam <eallam@icloud.com>
2023-03-07 11:48:50 +0000

    Update 4

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,6 @@ new Trigger({
   async run(event, ctx) {
     await ctx.waitFor("waiting...", { seconds: 10 });
 
-    await ctx.logger.info("Hello world from inside trigger.dev 3");
+    await ctx.logger.info("Hello world from inside trigger.dev 4");
   },
 }).listen();
